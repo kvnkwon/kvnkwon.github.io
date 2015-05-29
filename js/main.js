c30c33bc488feb32bff00ff32043bb96d34d0fbf
Kevin <kvnkwon@gmail.com>
2015-05-29 17:59:40 -0400

    Add jquery

---
+++ js/main.js
@@ -0,0 +1,39 @@
+$(document).ready(function() {
+
+  $('.vidstock-img').hover(
+    function () {
+    $(".vidstock-title").show();
+    },
+    function () {
+      $(".vidstock-title").hide();
+    }
+  );
+
+  $('.reso-img').hover(
+    function () {
+    $(".reso-title").show();
+    },
+    function () {
+      $(".reso-title").hide();
+    }
+  );
+
+  $('.pulsar-img').hover(
+    function () {
+    $(".pulsar-title").show();
+    },
+    function () {
+      $(".pulsar-title").hide();
+    }
+  );
+
+  $('.coach-img').hover(
+    function () {
+    $(".coach-title").show();
+    },
+    function () {
+      $(".coach-title").hide();
+    }
+  );
+
+});
