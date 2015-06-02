1955cd05d29530a2707b9f2de07611bad419828a
Kevin <kvnkwon@gmail.com>
2015-06-02 15:16:16 -0400

    Polish links

--- js/main.js
+++ js/main.js
@@ -36,4 +36,9 @@ $(document).ready(function() {
     }
   );
 
+  $('a[href*=#]').on('click', function(event){
+    event.preventDefault();
+    $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
+});
+
 });
