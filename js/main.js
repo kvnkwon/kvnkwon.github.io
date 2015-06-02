666d36a09f2e5b200bdbcf5945b23a5dc7214a25
Kevin <kvnkwon@gmail.com>
2015-06-02 15:55:31 -0400

    Add delay and easing to animation

--- js/main.js
+++ js/main.js
@@ -39,6 +39,7 @@ $(document).ready(function() {
   $('a[href*=#]').on('click', function(event){
     event.preventDefault();
     $('html,body').animate({scrollTop:$(this.hash).offset().top}, 500);
-});
+  });
+
 
 });
