9d9af1c5e93c5429152db66cf4dce9d01e77500a
Kevin Kwon <kevin@Kevins-MBP.readisuite.com>
2017-05-01 15:47:59 -0700

    Update vr skills

--- js/main.js
+++ js/main.js
@@ -1,21 +1,5 @@
 $(document).ready(function() {
 
-  $(".identity").typed({
-    strings: ["a developer", "a filmmaker", "an artist", "a designer", "a creative thinker", "a tech enthusiast", "an avid gamer", "a basketball lover", "a big dream chaser", "building the next big thing"],
-    typeSpeed: 1
-  });
-
    window.setHashLink = 0;
-
-  $(".project-tab").hover(
-    function() {
-      $(this).css('color', '#333');
-      $(this).parent().css('background-color', '#fff');
-    }, function () {
-      $(this).css('color', '#fff');
-      $(this).parent().css('background-color', 'transparent');
-    }
-
-  );
 
 });
