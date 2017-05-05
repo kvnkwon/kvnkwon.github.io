da84c10eeae5006185ddc25fafc8177fbebb5a53
Kevin Kwon <kevin@Kevins-MBP.readisuite.com>
2017-05-04 17:23:36 -0700

    Change portrait

--- js/main.js
+++ js/main.js
@@ -2,4 +2,10 @@ $(document).ready(function() {
 
    window.setHashLink = 0;
 
+   $(".home-body-image").mouseover(function () {
+      $(this).attr('src', $(this).data("hover"));
+    }).mouseout(function () {
+      $(this).attr('src', $(this).data("src"));
+    });
+
 });
